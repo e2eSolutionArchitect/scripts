cd2d598555f8605a59bd9f2923ac2119bd52eb6c
e2e Solution Architect <62712515+awstechguide@users.noreply.github.com>
2022-11-17 10:15:08 -0500

    Update add-a-new-user-to-ssh-ec2.sh

--- aws/ec2/add-a-new-user-to-ssh-ec2.sh
+++ aws/ec2/add-a-new-user-to-ssh-ec2.sh
@@ -1,5 +1,7 @@
+# This is to add multiple users to SSH an EC2
+# The primary user (who has the ssh key pair ) loginto the instance and run below commands to add another user for ssh access to that EC2.
+# The new user need to share private-key of his/her key <ssh-private-key-of-the-new-user>
 
-```
 sudo bash
 useradd -m --group sudo <username>
 mkdir -p /home/<username>/.ssh
@@ -7,5 +9,3 @@ echo ssh-rsa <ssh-private-key-of-the-new-user> >> /home/<username>/.ssh/authoriz
 chmod 700 /home/<username>/.ssh
 chmod 600 /home/<username>/.ssh/authorized_keys
 chown <username>:<username> -R /home/<username>/.ssh
-
-```
