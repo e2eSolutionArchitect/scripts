92a0e1483fef7ed85f8a3928f58a036b3f71f5b9
e2e Solution Architect <62712515+awstechguide@users.noreply.github.com>
2023-03-07 21:13:36 -0500

    Update python-call-rest-api.py

--- python/python-call-rest-api.py
+++ python/python-call-rest-api.py
@@ -1,6 +1,27 @@
-
 # GET call
+
+--------------------------
+# Option 1
+
 import requests
 api_url = "https://lnvajpvyae.execute-api.us-east-1.amazonaws.com/prod/message"
 response = requests.get(api_url)
 response.json()
+
+--------------------------
+
+# Option 2
+
+import requests
+import json
+
+responses = list() 
+
+for i in range(10):
+  print(i)
+  api_url = "https://lnvajpvyae.execute-api.us-east-1.amazonaws.com/prod/message"
+  responses = requests.get(api_url)
+  data=json.loads(responses.text)
+  print(data)
+  
+  --------------------------
