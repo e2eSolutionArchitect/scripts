f4c051dd37b6b177186f642269a6ca72da2cd3ad
The Solution Architect <62712515+TheSolutionArchitect@users.noreply.github.com>
2023-09-17 09:37:55 -0400

    Create userdata-kubectl-vm.sh

---
+++ aws/ec2/shell-scripts/userdata-kubectl-vm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -xe
+echo 'Starting Update'
+sudo apt-get update -y
+sudo apt-get install unzip
+echo 'Installing AWSCLI2'
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
+echo 'Installing kubectl'
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.4/2023-08-16/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+kubectl version --short --client
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
