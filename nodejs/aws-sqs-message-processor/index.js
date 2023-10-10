ed3c1cb4b2d96724f64a7a663c9f4555c0834cc2
Som <8308302+somspace@users.noreply.github.com>
2023-10-10 12:01:59 -0400

    Update index.js

--- nodejs/aws-sqs-message-processor/index.js
+++ nodejs/aws-sqs-message-processor/index.js
@@ -15,16 +15,14 @@ function generateUniqueId() {
   return uuidv4();
 }
 
-// Usage example
 const fileId = generateUniqueId();
-//console.log('Generated Message ID:', fileId);
 
 
 const SendMessageToQueue = async (body) => {
     try{
         const command = new SendMessageCommand({
             MessageBody: body,
-            QueueUrl: process.env.AWS_SQS_QUEUE_URL,
+            QueueUrl: process.env.AWS_SQS_QUEUE_URL_AWAITING,
             MessageAttributes:{
                 FileId: {DataType: "String", StringValue: fileId},
                 FileName: {DataType: "String", StringValue: "test.png"},
@@ -38,25 +36,25 @@ const SendMessageToQueue = async (body) => {
     }
 };
 
-//SendMessageToQueue("My first message to the world - fileId: "+fileId)
+SendMessageToQueue("File scan request for fileId: "+fileId)
 
 const PullMessagesFromQueue = async () => {
     try{
         const command = new ReceiveMessageCommand({
             MaxNumberOfMessages: 10,
-            QueueUrl: process.env.AWS_SQS_QUEUE_URL,
+            QueueUrl: process.env.AWS_SQS_QUEUE_URL_AWAITING,
             WaitTimeSeconds: 5,
             MessageAttributes: ["All"],
             VisibilityTimeout: 10,
 
         });
         const result = await sqsClient.send(command);
-        console.log(result.Messages);
+        //console.log(result.Messages);
 
         // do some message processing 
-        // processMessage(msgAttributes,result.Messages)
+        ProcessMessage(result);
         // delete the message after successful processing
-        const del_result = await DeleteMessageFromQueue(result.Messages[0].ReceiptHandle);
+        //const del_result = await DeleteMessageFromQueue(result.Messages[0].ReceiptHandle);
     } catch (error) {
         console.log(error);
     }
@@ -67,10 +65,23 @@ PullMessagesFromQueue();
 const DeleteMessageFromQueue = async (ReceiptHandle) => {
     try{
         const data = await sqsClient.send(new DeleteMessageCommand({
-            QueueUrl: process.env.AWS_SQS_QUEUE_URL,
+            QueueUrl: process.env.AWS_SQS_QUEUE_URL_AWAITING,
             ReceiptHandle: ReceiptHandle,
         }))
         console.log("deleted successfully......");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+
+const ProcessMessage = async (result) => {
+    try{
+        // read the message attributes and pull the file from s3 bucket
+        console.log('calling ProcessMessage');
+        console.log(result.Messages);
+        // post processing delete the current message and recreate a copy with status:scanned attribute to SCANNED queue
     } catch (error) {
         console.log(error);
     }
