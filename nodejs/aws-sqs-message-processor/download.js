f8ceafecba06669bb1a5b502d59643a6d1a2eb3d
The Solution Architect <62712515+TheSolutionArchitect@users.noreply.github.com>
2023-10-10 11:32:03 -0400

    Add files via upload

---
+++ nodejs/aws-sqs-message-processor/download.js
@@ -0,0 +1,32 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+require('dotenv').config();
+
+// Configure AWS SDK
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_ACCESS_SECRET_KEY,
+    region: process.env.AWS_REGION
+  });
+
+const s3 = new AWS.S3();
+
+const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: "uploads/fileup.pdf"
+  };
+  
+  s3.getObject(params, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  
+    fs.writeFile(process.env.LOCAL_PATH_TO_SAVE_FILE, data.Body, (writeErr) => {
+      if (writeErr) {
+        console.error(writeErr);
+        return;
+      }
+      console.log(`File downloaded to ${process.env.LOCAL_PATH_TO_SAVE_FILE}`);
+    });
+  });
--- nodejs/aws-sqs-message-processor/readme.md
+++ nodejs/aws-sqs-message-processor/readme.md
@@ -1,13 +1,14 @@
-
-```
 npm install
+
 npm init
 npm i @aws-sdk/client-sqs
 npm install --save dotenv 
 npm install uuid // to generate unique id for message id 
+npm install aws-sdk
+
 
 node producer.js
 node consumer.js
-```
 
+https://www.youtube.com/watch?v=_20HXT43hFk
 
